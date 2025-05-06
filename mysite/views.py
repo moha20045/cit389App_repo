5a188d630e1be1d2916ba2383d1143e4f3e62816
mohamed maidan <mohamed.maidan@atlantisuniversity.edu>
2025-05-06 00:58:48 -0400

    Updated views.py with custom Elastic Beanstalk messages

--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,7 @@
 from django.http import HttpResponse
 def home(request):
-    return HttpResponse("Hello from Elastic Beanstalk Django App!")
+    return HttpResponse("""
+    Hello from Elastic Beanstalk Django App!
+    Mohamed maidan Hello from Elastic Beanstalk django App! <br>
+    Atlantis university Hello from Elastic Beanstalk Django App!
+""")
