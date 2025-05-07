ebba966e8b155cf3cd45f4be1afb4627af6a8044
mohamed maidan <mohamed.maidan@atlantisuniversity.edu>
2025-05-07 02:30:10 -0400

    Add homepage template and view

--- mysite/settings.py
+++ mysite/settings.py
@@ -2,34 +2,18 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'replace-me'
-
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
-
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
 ]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-]
-
-ROOT_URLCONF = 'mysite.urls'
-
-WSGI_APPLICATION = 'mysite.wsgi.application'
-
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
--- mysite/views.py
+++ mysite/views.py
@@ -1,8 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("""
-    Hello from Elastic Beanstalk Django App!<br>
-    Mohamed Maidan Hello from Elastic Beanstalk django App!<br>
-    Atlantis university Hello from Elastic Beanstalk Django App!
-    """)
+    return render(request, 'home.html')
