e3d8f86509cc37960d0829fc2bbbf6d8d3643f28
mohamed maidan <mohamed.maidan@atlantisuniversity.edu>
2025-05-07 02:21:41 -0400

    Update settings for Elastic Beanstalk deployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,11 +1,35 @@
 from pathlib import Path
+
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 SECRET_KEY = 'replace-me'
+
 DEBUG = True
+
 ALLOWED_HOSTS = ['*']
-INSTALLED_APPS = ['django.contrib.contenttypes','django.contrib.staticfiles']
-MIDDLEWARE = ['django.middleware.security.SecurityMiddleware']
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+]
+
 ROOT_URLCONF = 'mysite.urls'
+
 WSGI_APPLICATION = 'mysite.wsgi.application'
+
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
